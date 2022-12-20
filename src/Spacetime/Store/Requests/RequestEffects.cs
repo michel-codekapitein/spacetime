bef4ee70976d6c97e46a6e89cc13c66219618210
michel-codekapitein <michel@codekapitein.nl>
2022-12-20 21:24:01 +0100

    Fix ambiguous reference between Microsoft.Maui.Dispatching.IDispatcher and Fluxor.IDispatcher

--- src/Spacetime/Store/Requests/RequestEffects.cs
+++ src/Spacetime/Store/Requests/RequestEffects.cs
@@ -22,14 +22,14 @@ namespace Spacetime.Store.Requests
         }
 
         [EffectMethod(typeof(FetchRequestsAction))]
-        public async Task HandleFetchRequestsAction(IDispatcher dispatcher)
+        public async Task HandleFetchRequestsAction(Fluxor.IDispatcher dispatcher)
         {
             var requests = await _requests.GetRequests();
             dispatcher.Dispatch(new FetchRequestsSuccessAction(requests.ToList()));
         }
 
         [EffectMethod]
-        public Task HandleDeleteRequestAction(DeleteRequestAction action, IDispatcher dispatcher)
+        public Task HandleDeleteRequestAction(DeleteRequestAction action, Fluxor.IDispatcher dispatcher)
         {
             _requests.DeleteRequest(action.Id);
             dispatcher.Dispatch(new DeleteRequestSuccessAction(action.Id));
@@ -38,25 +38,25 @@ namespace Spacetime.Store.Requests
         }
 
         [EffectMethod]
-        public async Task HandleAddRequestAction(AddRequestAction action, IDispatcher dispatcher)
+        public async Task HandleAddRequestAction(AddRequestAction action, Fluxor.IDispatcher dispatcher)
         {
             await _requests.AddRequest(action.Request);
             dispatcher.Dispatch(new AddRequestSuccessAction(action.Request));
         }
 
         [EffectMethod]
-        public async Task HandleUpdateRequestAction(UpdateRequestAction action, IDispatcher dispatcher)
+        public async Task HandleUpdateRequestAction(UpdateRequestAction action, Fluxor.IDispatcher dispatcher)
         {
             await _requests.UpdateRequest(action.Request);
             dispatcher.Dispatch(new UpdateRequestSuccessAction(action.Request));
         }
 
         [EffectMethod]
-        public async Task HandleExecuteRequestAction(ExecuteRequestAction action, IDispatcher dispatcher)
+        public async Task HandleExecuteRequestAction(ExecuteRequestAction action, Fluxor.IDispatcher dispatcher)
         {
             try
             {
-                var response = await _spacetime.Execute(action.Request);
+                var response = await _spacetime.Execute(action.Request, action.ResponseOptions);
                 dispatcher.Dispatch(new ExecuteRequestSuccessAction(action.Request.Id, response));
             }
             catch (Exception ex)
